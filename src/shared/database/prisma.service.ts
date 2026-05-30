3c8d68e50fde1903d4784d3e8d4cb27a39d5303e
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-05-30 11:57:50 -0300

    feat(prisma): add Prisma service/module

---
+++ src/shared/database/prisma.service.ts
@@ -0,0 +1,19 @@
+﻿import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import type { IEnvProvider } from '../env';
+import { ENV_PROVIDER } from '../env/env.module';
+import { PrismaClient } from '@/generated/prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor(@Inject(ENV_PROVIDER) env: IEnvProvider) {
+    const adapter = new PrismaPg({
+      connectionString: env.getOrThrow('DATABASE_URL'),
+    });
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
