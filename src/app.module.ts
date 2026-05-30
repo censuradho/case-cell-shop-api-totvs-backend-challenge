4b1cae31d82f59f0eb76d2ed24fcd69c0cd45c9d
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-05-30 11:10:21 -0300

    chore(CacheModule): set initial configuration using @keyv/redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { createKeyv } from '@keyv/redis';
+import { CacheModule } from '@nestjs/cache-manager';
+import { ENV_PROVIDER } from './shared/env/env.module';
+import { IEnvProvider } from './shared/env';
 
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.registerAsync({
+      isGlobal: true,
+      inject: [ENV_PROVIDER],
+      useFactory: (env: IEnvProvider) => ({
+        stores: [createKeyv(env.getOrThrow('REDIS_URL'))],
+        ttl: 1 * 60 * 1000,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
